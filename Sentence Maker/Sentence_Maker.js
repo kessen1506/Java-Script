145471476064452c493ed5775bbce70304cd38a4
Jhames Kessen <callmejhames@gmail.com>
2026-05-12 23:50:03 +0800

    Add files via upload

---
+++ Sentence Maker/Sentence_Maker.js
@@ -0,0 +1,17 @@
+let adjective = "Blue";
+let noun = "Dog";
+let verb = "Run";
+let place = "Paris";
+let adjective2 = "Red";
+let noun2 = "film";
+
+const firstStory = "Once upon a time there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " i live in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log(firstStory);
+
+adjective = "yellow";
+noun = "Cat";
+verb = "Walk";
+place = "Manila";
+adjective2 = "Green";
+noun2 = "film";
