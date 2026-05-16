5b1cace4d10ca82330f257ff84481345a4bfe9e1
Jhames Kessen <callmejhames@gmail.com>
2026-05-16 21:48:02 +0800

    Add files via upload

--- Sentence Maker/Sentence_Maker.js
+++ Sentence Maker/Sentence_Maker.js
@@ -7,7 +7,7 @@ let noun2 = "film";
 
 const firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
 
-console.log("First story: " + firstStory);
+console.log("First story:", firstStory);
 
 adjective = "yellow";
 noun = "Cat";
@@ -15,7 +15,3 @@ verb = "Walk";
 place = "Manila";
 adjective2 = "Green";
 noun2 = "film";
-
-const secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
-
-console.log("Second story: " + secondStory);
